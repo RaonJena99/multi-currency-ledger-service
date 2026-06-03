fa4bf8190a0bb4fe79305bb2719be3290a53e846
RaonJena99 <yejunjaj@gmail.com>
2026-06-04 00:12:42 +0900

    feat(common): JPA Auditing 기반 엔티티 및 전역 예외 처리기 구현

---
+++ src/main/java/com/github/raonjena99/multi_currency_ledger_service/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.github.raonjena99.multi_currency_ledger_service.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
---
+++ src/main/java/com/github/raonjena99/multi_currency_ledger_service/common/domain/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.github.raonjena99.multi_currency_ledger_service.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false, columnDefinition = "TIMESTAMPTZ")
+    private OffsetDateTime createdAt;
+}
