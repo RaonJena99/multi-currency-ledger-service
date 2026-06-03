2975dd1f3b6b52da830537359b57209732d318ba
RaonJena99 <yejunjaj@gmail.com>
2026-06-04 00:12:42 +0900

    feat(common): JPA Auditing 기반 엔티티 및 전역 예외 처리기 구현

---
+++ src/main/java/com/github/raonjena99/multi_currency_ledger_service/common/exception/ErrorResponse.java
@@ -0,0 +1,4 @@
+package com.github.raonjena99.multi_currency_ledger_service.common.exception;
+
+public record ErrorResponse(String code, String message) {
+}
---
+++ src/main/java/com/github/raonjena99/multi_currency_ledger_service/common/model/AssetType.java
@@ -0,0 +1,5 @@
+package com.github.raonjena99.multi_currency_ledger_service.common.model;
+
+public enum AssetType {
+    KRW, USD, BTC, ETH
+}
