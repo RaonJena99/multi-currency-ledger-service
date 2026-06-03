f39f9d7c25a1ca10fb4afd6407d4c421e4f8f16f
RaonJena99 <yejunjaj@gmail.com>
2026-06-04 00:12:42 +0900

    feat: implement JPA auditing base entity and global exception handler

---
+++ src/main/java/com/github/raonjena99/multi_currency_ledger_service/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.github.raonjena99.multi_currency_ledger_service.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse("INVALID_INPUT", e.getMessage()));
+    }
+
+}
