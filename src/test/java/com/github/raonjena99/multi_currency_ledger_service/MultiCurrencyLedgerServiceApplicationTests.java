c0723e27b61157d49315620945068ef12c76951a
RaonJena99 <yejunjaj@gmail.com>
2026-06-04 00:13:05 +0900

    test: establish testcontainers integration test infrastructure

---
+++ src/test/java/com/github/raonjena99/multi_currency_ledger_service/MultiCurrencyLedgerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.raonjena99.multi_currency_ledger_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiCurrencyLedgerServiceApplicationTests extends IntegrationTestSupport {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
