836e4ffc9c484d4381a1f3c0ca6f2d60629097da
RaonJena99 <yejunjaj@gmail.com>
2026-06-10 03:43:39 +0900

    chore(common): 전역 예외 처리기 강화 및 JPA Auditing 설정 최적화

--- src/main/java/com/github/raonjena99/multi_currency_ledger_service/common/config/JpaAuditingConfig.java
+++ src/main/java/com/github/raonjena99/multi_currency_ledger_service/common/config/JpaAuditingConfig.java
@@ -1,9 +1,19 @@
 package com.github.raonjena99.multi_currency_ledger_service.common.config;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "offsetDateTimeProvider")
 public class JpaAuditingConfig {
+
+    @Bean
+    public DateTimeProvider offsetDateTimeProvider() {
+        return () -> Optional.of(OffsetDateTime.now());
+    }
 }
